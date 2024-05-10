75b7e983a00982b5f8c98a374b002f381f4fe241
Kadda Aoues <houes_k@yahoo.fr>
2024-05-10 17:53:52 +0300

    create new library build on top of asio.h

--- Asio_exemple/Asio_Exemple/asio_utillity.h
+++ Asio_exemple/Asio_Exemple/asio_utillity.h
@@ -1,13 +1,4 @@
 #pragma once
 
-#define  now_time           std::chrono::system_clock::now()
-#define  time_now           now_time
-#define  Seconds(n)         std::chrono::seconds(n)
-#define  Milli(n)           std::chrono::milliseconds(n)
-#define  Sleep(duration)    std::this_thread::sleep_for(duration)   
-#define  ThreadId           std::this_thread::get_id()
+#include <MyLib/Asio_tools/asio_utility.h>
 
-//#define in
-//#define out
-
-#define  _CTRL_C            3
